ba775dc161979a9cb0790e78e9acedcc5c705f15
Thirumalai Nayagam.K <85454295+ThiruUzumaki13@users.noreply.github.com>
2022-07-27 23:00:07 +0530

    Add files via upload

---
+++ Project/MEGA/esp/esp.ino
@@ -0,0 +1,56 @@
+#include <PubSubClient.h>
+
+#include <UbidotsESPMQTT.h>
+
+/****************************************
+ * Include Libraries
+ ****************************************/
+#include "UbidotsESPMQTT.h"
+
+/****************************************
+ * Define Constants
+ ****************************************/
+#define TOKEN "BBFF-hI3JqjUf94m1uD3qtdtovOHbF43OUY" // Your Ubidots TOKEN
+#define WIFINAME "project12345" //Your SSID
+#define WIFIPASS "project123" //Your Wifi Pass
+
+Ubidots client(TOKEN);
+
+/****************************************
+ * Auxiliar Functions
+ ****************************************/
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+/****************************************
+ * Main Functions
+ ****************************************/
+
+void setup() {
+  // put your setup code here, to run once:
+  //client.ubidotsSetBroker("industrial.api.ubidots.com"); // Sets the broker properly for the industrial account
+  client.setDebug(true); // Pass a true or false bool value to activate debug messages
+  Serial.begin(115200);
+  client.wifiConnection(WIFINAME, WIFIPASS);
+  client.begin(callback);
+  }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(!client.connected()){
+      client.reconnect();
+      }
+  
+  float value1 = analogRead(A0);
+  client.add("temperature", value1);
+  client.ubidotsPublish("my-new-device");
+  client.loop();
+}
